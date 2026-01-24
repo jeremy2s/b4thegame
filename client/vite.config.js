397fefd3a0817d391c14c88033e875a39d16cb6f
Jeremy Tews <jeremytews@Jeremys-MacBook-Pro.local>
2026-01-24 11:49:15 -0600

    Deploy to gh-pages

--- client/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-// Minimal Vite config for client-only mode
-export default defineConfig(() => ({
-  base: process.env.GITHUB_ACTIONS ? '/b4thegame/' : '/',
-  server: {
-    // no proxy necessary in client-only mode
-  }
-}))
